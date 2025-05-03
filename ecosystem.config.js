cd28f663653f34ea7e811fcdb0d40496c9bb79f3
Yuusuke Ishii <111514756+Sanchi7412@users.noreply.github.com>
2025-05-03 13:47:30 +0900

    Create ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-    apps: [
+  apps: [
     {
-        name: "nekoniwa-network",
-        script: "pnpm",
-        args: "start",
-        cwd: "/home/ubuntu/nekoniwa-network",  // アプリのディレクトリを指定
-        interpreter: "/bin/bash",  // 必要ならシェルを指定
-        exec_mode: "fork",
-        env: {
-            NODE_ENV: "production",
-        },
+      name: "nekoniwa-network",
+      script: "pnpm",
+      args: "start",
+      cwd: "/home/ubuntu/nekoniwa-network", // アプリのディレクトリを指定
+      interpreter: "/bin/bash", // 必要ならシェルを指定
+      exec_mode: "fork",
+      env: {
+        NODE_ENV: "production",
+      },
     },
-    ],
+  ],
 };
