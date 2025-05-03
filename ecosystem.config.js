9f26bc275f2b85cf11bcdc21caac7319927e1276
Yuusuke Ishii <111514756+Sanchi7412@users.noreply.github.com>
2025-05-03 13:50:57 +0900

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,17 @@
-module.exports = {
+import { defineConfig } from "pm2";
+
+export default defineConfig({
   apps: [
     {
       name: "nekoniwa-network",
       script: "pnpm",
       args: "start",
-      cwd: "/home/ubuntu/nekoniwa-network", // アプリのディレクトリを指定
-      interpreter: "/bin/bash", // 必要ならシェルを指定
+      cwd: "/home/ubuntu/nekoniwa-network",
+      interpreter: "/bin/bash",
       exec_mode: "fork",
       env: {
         NODE_ENV: "production",
       },
     },
   ],
-};
+});
